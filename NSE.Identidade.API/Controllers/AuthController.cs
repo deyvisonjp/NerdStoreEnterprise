a201ed7f0a49a84bbd53bc323dc10bc8d534f031
deyvisonjp <deyvisonjp@id.uff.br>
2024-12-01 12:43:33 -0300

    Adicionar arquivos de projeto.

---
+++ NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Extensions;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static NSE.Identidade.API.Models.UserViewModels;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identidade")]
+    [ApiController]
+    public class AuthController : MainController
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly AppSettings _appSettings;
+
+        public AuthController(UserManager<IdentityUser> userManager, 
+                                SignInManager<IdentityUser> signInManager, 
+                                IOptions<AppSettings> appSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(NewUser usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Password);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(await GerarJwt(usuarioRegistro.Email));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                AdicionarErroProcessamento(error.Description);
+            }
+
+            return CustomResponse(result);
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UserLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Password, 
+                false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(await GerarJwt(usuarioLogin.Email));
+            }
+
+            if (result.IsLockedOut) 
+            {
+                AdicionarErroProcessamento("Usuario temporariamente bloqueado por tentativas inválidas.");
+                return CustomResponse();
+            }
+
+            AdicionarErroProcessamento("Usuário ou Senha incorretos.");
+            return CustomResponse();
+        }
+
+        private async Task<UserLoginResponse> GerarJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            // Guid para o Token
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            // Quando Expira
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            // Quando foi Emitido
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var useRole in userRoles)
+            {
+                claims.Add(new Claim("role", useRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            // Token Codificado
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            var response = new UserLoginResponse
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserToken
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UserClaim { Type = c.Type, Value = c.Value })
+
+                }
+            };
+
+            return response;
+
+        }
+
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1790, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto Nerd Stored Enterprise
+
+_Projeto criado em .net para estudos pessoais com o curso ministrado pelo "desenvolvedor.io"_
+
+## Design de pastas
+- Building Blocks: Distribuição de utilidades
+- Services: As APIs
+- Web: Projeto MVC, poderiamos usar um SPA, mas iniciamente usaremos o front com .net
+
+## Boa Prática - API Authenticator
+1. Criar uma controller Base: Métodos compartilhaveis (MainController)
